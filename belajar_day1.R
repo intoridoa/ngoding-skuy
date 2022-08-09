bee077a142b02f9a46e7daf31442333c86fab66c
intoridoa <109396506+intoridoa@users.noreply.github.com>
2022-08-09 12:07:37 +0700

    Create belajar_day1.R

---
+++ belajar_day1.R
@@ -0,0 +1,13 @@
+#A. Introduction to Data Science with R
+# 1. untuk komentar gunakan "#"
+
+# 2. Vector merupakan sebuah struktur data yang dapat menyimpan lebih dari satu data yang akan digunakan di R.
+c(3,10,15)
+c(1:5)
+
+# 3. summary
+#Kesimpulan ini berupa nilai statistik deskriptif dari data yang dimiliki, 
+#diantaranya nilai minimum, nilai kuartil pertama, nilai tengah (median), nilai kuartil ketiga, dan nilai maksimum data.
+summary(c(1:5))
+
+#B. 
