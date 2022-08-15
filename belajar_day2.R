0d89e4a848a887d36b28887ef5d6a6836669a665
intoridoa <109396506+intoridoa@users.noreply.github.com>
2022-08-15 19:15:41 +0700

    Create belajar_day2.R

---
+++ belajar_day2.R
@@ -0,0 +1,18 @@
+#Bismillah
+#Bahasa pemograman R bersifat case sensitive
+
+#print -> berfungsi menampilkan data
+
+c(5:10) #Membuat function yang menampilkan 5-10
+
+#Membuat variable
+berat_badan_kg <- 95
+#Menampilkan isi variable
+beart_badan_kg
+
+# Komentar menggunakan tanda "#"
+
+#Vektor
+#Definisi: Vector merupakan tipe data sederhana di R yang menyimpan deretan nilai (lebih dari satu nilai) dengan tipe data sama untuk setiap elemennya.
+# jika tipe datanya berupa teks maka seluruh elemennya harus bertipe teks. Demikian juga jika tipenya angka maka seluruh elemennya berisi angka semua
+
